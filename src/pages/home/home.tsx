f71871014c9b7fc587c8793279b04f6c8404debb
Michaeldam88 <michaeldam88@gmail.com>
2022-12-26 11:24:25 +0100

    added the logo and text

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,10 @@
-export function Home () {
+export function Home() {
+    const totalRobots = 'XXX';
     return (
         <section>
             <h2>Home</h2>
+            <img src="https://robohash.org/factory.png"></img>
+            <p>Total robots disponibles {totalRobots}</p>
         </section>
     );
 }
