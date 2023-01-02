30d3e2fa48c3c817a3ab55816e46631751681ac0
Michaeldam88 <michaeldam88@gmail.com>
2023-01-02 16:06:10 +0100

    added the server configuration

--- src/services/repository/robots.repo.ts
+++ src/services/repository/robots.repo.ts
@@ -4,8 +4,7 @@ import { RobotsStructure } from "../../types/robot";
 const invalidIdError = new Error('Invalid ID');
 
 export class RobotsRepo implements Repository<RobotsStructure> {
-    constructor(private url = 'http://localhost:3300/robots/') {
-    }
+    constructor(private url = 'https://test-api-9l5y.onrender.com/robots/') {}
 
     async load(): Promise<RobotsStructure[]> {
         const resp = await fetch(this.url);
@@ -34,7 +33,7 @@ export class RobotsRepo implements Repository<RobotsStructure> {
             throw new Error(`Error ${resp.status}: ${resp.statusText}`);
         return await resp.json();
     }
-    
+
     async update(payload: Partial<RobotsStructure>): Promise<RobotsStructure> {
         if (!payload.id) return Promise.reject(invalidIdError);
         const resp = await fetch(this.url + payload.id, {
