9aa52cf50d5289a6f61555e86bbda16470d0e797
Michaeldam88 <michaeldam88@gmail.com>
2023-01-09 14:12:06 +0100

    added test to home

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -15,4 +15,32 @@ describe('Given Home component', () => {
         const totalRobotsParagraph = screen.getByTestId('totalRobots');
         expect(totalRobotsParagraph).toBeInTheDocument();
     });
+
+    test('if there is no info in session storage should show Total robots disponibles 0', () => {
+        const totalRobotsParagraph = screen.getByTestId('totalRobots');
+        expect(totalRobotsParagraph).toBeInTheDocument();
+        sessionStorage.clear();
+        const noRobotsParagraph = screen.getByText(
+            'Total robots disponibles 0'
+        );
+        expect(noRobotsParagraph).toBeInTheDocument();
+    });
+});
+
+describe('Given sessin storage with data', () => {
+    sessionStorage.setItem('totalRobots', JSON.stringify(2));
+    beforeEach(() => {
+        render(
+            <BrowserRouter>
+                <Home />
+            </BrowserRouter>
+        );
+    });
+
+    test('if there is 2 robots in session storage should show the correct number', () => {
+        const noRobotsParagraph = screen.getByText(
+            'Total robots disponibles 2'
+        );
+        expect(noRobotsParagraph).toBeInTheDocument();
+    });
 });
